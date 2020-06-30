41e0084fc273d32725d333e7e42fa224d28852ac
Erick Agrazal <erick.agrazal@gmail.com>
2020-06-29 21:33:10 -0500

    Agregado eliminar y ver un solo estudiante.

--- app.js
+++ app.js
@@ -10,12 +10,41 @@ const Estudiantes = require('./models/Estudiantes');
 const PORT = 3000;
 
 
+// Configuraciones
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
 // Intermediarios
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cors());
 
 
-// Controladores
+// Controladores - Views
+app.get('/estudiantes/', async (req, res) => {
+    const estudiantes = await Estudiantes.find().select('nombre edad');
+    res.render('estudiantes', { estudiantes });
+});
+app.get('/estudiantes/:id', async (req, res) => {
+    try {
+        const estudiante = await Estudiantes.findById(req.params.id).select('nombre edad');
+        res.render('estudiantes_detail', { estudiante });
+    } catch (error) {
+        console.log(error);
+        res.send('Error');
+    }
+});
+app.post('/estudiantes', async (req, res) => {
+    const { nombre, edad } = req.body;
+    await Estudiantes.create({ nombre, edad });
+    const estudiantes = await Estudiantes.find().select('nombre edad');
+    res.render('estudiantes', { estudiantes, success: true });
+});
+
+// Controladores - API
 app.get('/api/estudiantes/', async (req, res) => {
     const estudiantes = await Estudiantes.find().select('nombre edad');
     res.json({
@@ -33,6 +62,15 @@ app.get('/api/estudiantes/:id', async (req, res) => {
     try {
         const estudiante = await Estudiantes.findById(req.params.id).select('nombre edad');
         res.json(estudiante);
+    } catch (error) {
+        console.log(error);
+        res.json({});
+    }
+});
+app.delete('/api/estudiantes/:id', async (req, res) => {
+    try {
+        await Estudiantes.findByIdAndDelete(req.params.id);
+        res.status(202).json({});
     } catch (error) {
         console.log(error);
         res.json({});
